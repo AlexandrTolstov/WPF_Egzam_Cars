733588ad6018334d794009aae2b4431af3cf1aa6
AlexandrTolstov <alextav_85@mail.ru>
2018-12-05 17:45:00 +0600

    Оформление меню

--- WPF_Egzam_Cars/MainWindow.xaml.cs
+++ WPF_Egzam_Cars/MainWindow.xaml.cs
@@ -31,18 +31,18 @@ namespace WPF_Egzam_Cars
             InitializeComponent();
         }
 
-        #region События кнопки Главное
+        #region События оформления кнопки Главное
         private void ButtonGlav_MouseEnter(object sender, MouseEventArgs e)
         {
             BorderHome.Background = ColorsMenu.YellowColor;
             BorderHome1.Background = ColorsMenu.YellowColor;
-            ImageHome.Source = new BitmapImage(new Uri(@"Menu_png\i.png", UriKind.RelativeOrAbsolute));
+            ImageHome.Source = new BitmapImage(new Uri(@"Menu_png\Home_b.png", UriKind.RelativeOrAbsolute));
         }
         private void ButtonGlav_MouseLeave(object sender, MouseEventArgs e)
         {
             BorderHome.Background = ColorsMenu.GrayColor;
             BorderHome1.Background = ColorsMenu.GrayColor;
-            ImageHome.Source = new BitmapImage(new Uri(@"Menu_png\Home.png", UriKind.RelativeOrAbsolute));
+            ImageHome.Source = new BitmapImage(new Uri(@"Menu_png\Home_g.png", UriKind.RelativeOrAbsolute));
         }
         private void ButtonGlav_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -53,6 +53,31 @@ namespace WPF_Egzam_Cars
         {
             BorderHome.Background = ColorsMenu.YellowColor;
             BorderHome1.Background = ColorsMenu.YellowColor;
+        }
+        #endregion
+
+        #region События оформления кнопки Отложенные работы
+        private void ButtonOtlRab_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BorderOtlRab.Background = ColorsMenu.YellowColor;
+            BorderOtlRab1.Background = ColorsMenu.YellowColor;
+            ImageOtlRab.Source = new BitmapImage(new Uri(@"Menu_png\information_b.png", UriKind.RelativeOrAbsolute));
+        }
+        private void ButtonOtlRab_MouseLeave(object sender, MouseEventArgs e)
+        {
+            BorderOtlRab.Background = ColorsMenu.GrayColor;
+            BorderOtlRab1.Background = ColorsMenu.GrayColor;
+            ImageOtlRab.Source = new BitmapImage(new Uri(@"Menu_png\information_g.png", UriKind.RelativeOrAbsolute));
+        }
+        private void ButtonOtlRab_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            BorderOtlRab.Background = ColorsMenu.OrangeColor;
+            BorderOtlRab1.Background = ColorsMenu.OrangeColor;
+        }
+        private void ButtonOtlRab_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            BorderOtlRab.Background = ColorsMenu.YellowColor;
+            BorderOtlRab1.Background = ColorsMenu.YellowColor;
         }
         #endregion
     }
