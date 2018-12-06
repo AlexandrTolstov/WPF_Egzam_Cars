c03207ce30e6344c8e08e44dc200da487ad91c87
AlexandrTolstov <alextav_85@mail.ru>
2018-12-06 16:21:42 +0600

    Левое меню

--- WPF_Egzam_Cars/MainWindow.xaml.cs
+++ WPF_Egzam_Cars/MainWindow.xaml.cs
@@ -31,53 +31,185 @@ namespace WPF_Egzam_Cars
             InitializeComponent();
         }
 
-        #region События оформления кнопки Главное
+        #region Кнопка Главное - смена иконок
         private void ButtonGlav_MouseEnter(object sender, MouseEventArgs e)
         {
-            BorderHome.Background = ColorsMenu.YellowColor;
-            BorderHome1.Background = ColorsMenu.YellowColor;
             ImageHome.Source = new BitmapImage(new Uri(@"Menu_png\Home_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrHome.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
         }
+
         private void ButtonGlav_MouseLeave(object sender, MouseEventArgs e)
         {
-            BorderHome.Background = ColorsMenu.GrayColor;
-            BorderHome1.Background = ColorsMenu.GrayColor;
             ImageHome.Source = new BitmapImage(new Uri(@"Menu_png\Home_g.png", UriKind.RelativeOrAbsolute));
-        }
-        private void ButtonGlav_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            BorderHome.Background = ColorsMenu.OrangeColor;
-            BorderHome1.Background = ColorsMenu.OrangeColor;
-        }
-        private void ButtonGlav_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            BorderHome.Background = ColorsMenu.YellowColor;
-            BorderHome1.Background = ColorsMenu.YellowColor;
+            ImageArrHome.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
         }
         #endregion
 
-        #region События оформления кнопки Отложенные работы
+        #region Кнопка Отложенные работы - смена иконок
         private void ButtonOtlRab_MouseEnter(object sender, MouseEventArgs e)
         {
-            BorderOtlRab.Background = ColorsMenu.YellowColor;
-            BorderOtlRab1.Background = ColorsMenu.YellowColor;
             ImageOtlRab.Source = new BitmapImage(new Uri(@"Menu_png\information_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrOtlRab.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
         }
+
         private void ButtonOtlRab_MouseLeave(object sender, MouseEventArgs e)
         {
-            BorderOtlRab.Background = ColorsMenu.GrayColor;
-            BorderOtlRab1.Background = ColorsMenu.GrayColor;
             ImageOtlRab.Source = new BitmapImage(new Uri(@"Menu_png\information_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrOtlRab.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
         }
-        private void ButtonOtlRab_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        #endregion
+
+        #region Кнопка Выполненные работы - смена иконок
+        private void ButtonVipRab_MouseEnter(object sender, MouseEventArgs e)
         {
-            BorderOtlRab.Background = ColorsMenu.OrangeColor;
-            BorderOtlRab1.Background = ColorsMenu.OrangeColor;
+            ImageVipRab.Source = new BitmapImage(new Uri(@"Menu_png\key_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrVipRab.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
         }
-        private void ButtonOtlRab_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+
+        private void ButtonVipRab_MouseLeave(object sender, MouseEventArgs e)
         {
-            BorderOtlRab.Background = ColorsMenu.YellowColor;
-            BorderOtlRab1.Background = ColorsMenu.YellowColor;
+            ImageVipRab.Source = new BitmapImage(new Uri(@"Menu_png\key_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrVipRab.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка Агрегат - смена иконок
+        private void ButtonAgreg_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageAgreg.Source = new BitmapImage(new Uri(@"Menu_png\Cogs_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrAgreg.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonAgreg_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageAgreg.Source = new BitmapImage(new Uri(@"Menu_png\Cogs_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrAgreg.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка Моточасы - смена иконок
+        private void ButtonMotochas_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageMotochas.Source = new BitmapImage(new Uri(@"Menu_png\clock_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrMotochas.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonMotochas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageMotochas.Source = new BitmapImage(new Uri(@"Menu_png\clock_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrMotochas.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка Расходные материалы - смена иконок
+        private void ButtonRashMat_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageRashMat.Source = new BitmapImage(new Uri(@"Menu_png\drop_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrRashMat.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonRashMat_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageRashMat.Source = new BitmapImage(new Uri(@"Menu_png\drop_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrRashMat.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка План - смена иконок
+        private void ButtonPlan_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImagePlan.Source = new BitmapImage(new Uri(@"Menu_png\calendar_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrPlan.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonPlan_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImagePlan.Source = new BitmapImage(new Uri(@"Menu_png\calendar_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrPlan.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка Проект - смена иконок
+        private void ButtonProect_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageProect.Source = new BitmapImage(new Uri(@"Menu_png\world_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrProect.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonProect_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageProect.Source = new BitmapImage(new Uri(@"Menu_png\world_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrProect.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка Список машин - смена иконок
+        private void ButtonListOfCars_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageListOfCars.Source = new BitmapImage(new Uri(@"Menu_png\CAR_B.png", UriKind.RelativeOrAbsolute));
+            ImageArrListOfCars.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonListOfCars_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageListOfCars.Source = new BitmapImage(new Uri(@"Menu_png\CAR_G.png", UriKind.RelativeOrAbsolute));
+            ImageArrListOfCars.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка Справочник - смена иконок
+        private void ButtonSprav_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageSprav.Source = new BitmapImage(new Uri(@"Menu_png\graph_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrSprav.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonSprav_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageSprav.Source = new BitmapImage(new Uri(@"Menu_png\graph_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrSprav.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка доступы - смена иконок
+        private void ButtonDost_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageDost.Source = new BitmapImage(new Uri(@"Menu_png\people_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrDost.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonDost_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageDost.Source = new BitmapImage(new Uri(@"Menu_png\people_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrDost.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка Checklist - смена иконок
+        private void ButtonCheck_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageCheck.Source = new BitmapImage(new Uri(@"Menu_png\magic_wand_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrCheck.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonCheck_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageCheck.Source = new BitmapImage(new Uri(@"Menu_png\magic_wand_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrCheck.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
+        }
+        #endregion
+
+        #region Кнопка Отчеты - смена иконок
+        private void ButtonReports_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ImageReports.Source = new BitmapImage(new Uri(@"Menu_png\paper_b.png", UriKind.RelativeOrAbsolute));
+            ImageArrReports.Source = new BitmapImage(new Uri(@"Menu_png\Arr.png", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ButtonReports_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ImageReports.Source = new BitmapImage(new Uri(@"Menu_png\paper_g.png", UriKind.RelativeOrAbsolute));
+            ImageArrReports.Source = new BitmapImage(new Uri(@"Menu_png\Arr_empt.png", UriKind.RelativeOrAbsolute));
         }
         #endregion
     }
